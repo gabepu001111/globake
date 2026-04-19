55e72024b1ff007bcc9ccf66656326a28cea58df
Gabe Puente <gabrielpuente35@gmail.com>
2026-04-19 01:57:45 -0400

    add

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/globake/',  
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
